f435ea22c0ca7fdd0d5c0c1ffafdde24ba18cf64
Brad Niedt <bradleyniedt@gmail.com>
2019-02-06 09:26:32 -0700

    Adding small changes to the test.

--- spec/features/user_can_search_spec.rb
+++ spec/features/user_can_search_spec.rb
@@ -5,7 +5,7 @@ describe 'As a user' do
     it 'should list the 10 closest electric and propane stations within 6 miles sorted by distance' do
       visit '/'
 
-      fill_in search[:q] with 80203
+      fill_in ".form-control", with: 80203
       click_link("Locate")
 
       expect(current_path).to eq('/search')
@@ -23,11 +23,11 @@ describe 'As a user' do
   end
 end
 
-As a user
-When I visit "/"
-And I fill in the search form with 80203 (Note: Use the existing search form)
-And I click "Locate"
-Then I should be on page "/search"
-Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-And the stations should be limited to Electric and Propane
-And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203 (Note: Use the existing search form)
+# And I click "Locate"
+# Then I should be on page "/search"
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times

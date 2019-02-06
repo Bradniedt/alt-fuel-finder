1075e695d637cbca9b443673337961c3f9f7385d
Brad Niedt <bradleyniedt@gmail.com>
2019-02-06 10:04:11 -0700

    Changes assertions in station test.

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Station, type: :model do
-  describe 'attributes' do
+  it 'attributes' do
     data = { station_name: "station1",
              street_address: "123 street",
              city: "denver",
@@ -9,14 +9,14 @@ describe Station, type: :model do
              zip: 80201,
              fuel_type_code: "ELEC",
              distance: 6,
-             access__days_time: "24hrs" }
+             access_days_time: "24hrs" }
     station = Station.new(data)
 
     expect(station).to be_an_instance_of Station
-    expect(station.name).to eq(data[:name])
-    expect(station.address).to eq(data[:address])
-    expect(station.fuel_types).to eq(data[:fuel_types])
+    expect(station.name).to eq(data[:station_name])
+    expect(station.address).to eq("#{data[:street_address]}, #{data[:city]}, #{data[:state]} #{data[:zip]}")
+    expect(station.fuel_types).to eq(data[:fuel_type_code])
     expect(station.distance).to eq(data[:distance])
-    expect(station.access_times).to eq(data[:access_times])
+    expect(station.access_times).to eq(data[:access_days_time])
   end
 end

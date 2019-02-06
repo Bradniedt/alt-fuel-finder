22464fafde967ec0d8d31630941115dd7b5f50d0
Brad Niedt <bradleyniedt@gmail.com>
2019-02-06 10:17:21 -0700

    Adds change to search controller to use the station find all method. Feature test passes.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,5 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://developer.nrel.gov/") do |f|
-      f.adapter Faraday.default_adapter
-    end
-    location = params[:q]
-    response = conn.get("api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['API_KEY']}&location=#{location}&&fuel_type=ELEC,LPG&radius=6&limit=10")
-
-    @stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations].map do |data|
-      Station.new(data)
-    end
+    @stations = Station.find_all(params[:q])
   end
 end

5901257b992360f4c8127921b7469c84aa612a6a
Brad Niedt <bradleyniedt@gmail.com>
2019-02-06 09:46:15 -0700

    Adds beginning of faraday adapter to search controller.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,16 @@
 class SearchController < ApplicationController
   def index
+    binding.pry
+    conn = Faraday.new(url: "https://developer.nrel.gov/") do |f|
+      f.headers[api_key] = ENV["API_KEY"]
+      f.adapter Faraday.default_adapter
+    end
+    location = params[:location]
+    response = conn.get("api/alt-fuel-stations/v1/nearest.json?location=#{location}&&fuel_type=ELEC,LPG&radius=6&limit=10")
+
+    @stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations].map do |data|
+      Station.new(data)
+    end
     
   end
 end

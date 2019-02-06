aea9c69deb853aa02a3ebff60010f8754f65e8dc
Brad Niedt <bradleyniedt@gmail.com>
2019-02-06 10:22:15 -0700

    Adds changes to refactor station service, still fixing.

--- app/services/station_service.rb
+++ app/services/station_service.rb
@@ -1,10 +1,18 @@
 class StationService
-  def self.find_stations(location)
-    conn = Faraday.new(url: "https://developer.nrel.gov/") do |f|
+  def initialize(location)
+    @url = "/v1/nearest.json?api_key=#{ENV['API_KEY']}&location=#{location}&&fuel_type=ELEC,LPG&radius=6&limit=10"
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations") do |f|
       f.adapter Faraday.default_adapter
     end
-    response = conn.get("api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['API_KEY']}&location=#{location}&&fuel_type=ELEC,LPG&radius=6&limit=10")
+  end
+
+  def find_stations
+    response = @conn.get(@url)
     results = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+  end
+
+  def self.find_stations(zip)
+    new(zip).find_stations
   end
 
 end

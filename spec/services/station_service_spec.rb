c2a9e63891d209ebe9ba4ebd06bb9a0e35916811
Brad Niedt <bradleyniedt@gmail.com>
2019-02-06 10:42:31 -0700

    Adds changes to station service test.

--- spec/services/station_service_spec.rb
+++ spec/services/station_service_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 describe StationService, type: :model do
-  it 'returns a collection of hash data' do
-    
+  it 'exists' do
+    service = StationService.new(80203)
+
+    expect(service).to be_an_instance_of StationService
+  end
+  it 'returns a collection of station data filtered by zip' do
+    hashes = StationService.find_stations(80203)
+    station = hashes.first
+
+    expect(hashes.count).to eq(10)
+    expect(station).to have_key(:station_name)
+    expect(station).to have_key(:address)
+    expect(station).to have_key(:city)
+    expect(station).to have_key(:state)
+    expect(station).to have_key(:zip)
+    expect(station).to have_key(:fuel_type_code)
+    expect(station).to have_key(:distance)
+    expect(station).to have_key(:access_days_time)
   end
 end
